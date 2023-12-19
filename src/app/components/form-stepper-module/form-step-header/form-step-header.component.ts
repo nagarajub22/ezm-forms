0dc76d9291693bc4405b639213f2ea797b850262
NagaRaju <nagaraju.b22@outlook.com>
2023-12-19 19:30:45 +0530

    add step click

--- src/app/components/form-stepper-module/form-step-header/form-step-header.component.ts
+++ src/app/components/form-stepper-module/form-step-header/form-step-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IFormStep } from '../models/form-stepper.interface';
 
 @Component({
@@ -9,6 +9,13 @@ import { IFormStep } from '../models/form-stepper.interface';
 export class FormStepHeaderComponent {
   @Input() title!: string;
   @Input({required: true}) steps!: IFormStep[];
-
   @Input() selectedStepIndex!: number;
+
+  @Output() emitStepClick: EventEmitter<number> = new EventEmitter();
+
+  onClickHeader(i: number) {
+    if(this.steps[i].completed || this.steps[i].editable) {
+      this.emitStepClick.emit(i);
+    }
+  }
 }
